b8b0c7a0bf99119513b6a15754c2db3b6980f4f8
unknown <marcelofelix.af@gmail.com>
2022-03-03 23:51:58 -0300

    added mushroom enemy

---
+++ Scripts/Mushroom.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+export var speed = 64
+export var healt = 1
+var motion = Vector2.ZERO
+var move_direction = -1
+var gravity = 1200
+var hitted = false
+
+func _physics_process(delta: float):
+	motion.x = speed * move_direction
+	motion.y += gravity * delta
+	
+	if move_direction == 1:
+		$texture.flip_h = true
+	else:
+		$texture.flip_h = false
+	
+	_set_animation()
+	
+	motion = move_and_slide(motion)
+
+
+func _on_anim_animation_finished(anim_name):
+	if(anim_name == "idle"):
+		$texture.flip_h != $texture.flip_h
+		$ray_wall.scale.x *= -1
+		move_direction *= -1
+		$anim.play("run")
+
+func _set_animation():
+	var anim = "run"
+	
+	if $ray_wall.is_colliding():
+		anim = "idle"
+	elif motion.x != 0:
+		anim = "run"
+	
+	if hitted:
+		anim = "hit"
+	
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
+
+func _on_hitbox_body_entered(body: Node):
+	hitted = true
+	healt -= 1
+	body.velocity.y -= 300
+	yield(get_tree().create_timer(0.2), "timeout")
+	hitted = false
+	if healt < 1:
+		queue_free()
+		get_node("hitbox/collision").set_deferred("disabled", true)
